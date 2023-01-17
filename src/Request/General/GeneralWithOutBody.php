2440faa9d4996999297ee1ef87d19c14231278b5
raviMukti <shinjiravi@gmail.com>
2023-01-17 21:46:15 +0700

    Enhancement class and method usage

---
+++ src/Request/General/GeneralWithOutBody.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Aftwork\TiktokShop\Request\General;
+
+use Aftwork\TiktokShop\Common\SignGenerator;
+use Aftwork\TiktokShop\Common\TiktokShopConfig;
+use Exception;
+use GuzzleHttp\Client;
+
+class GeneralWithOutBody
+{
+    /**
+     * @param $httpMethod
+     * @param $baseUrl
+     * @param $apiPath
+     * @param $params
+     * @param TiktokShopConfig $apiConfig
+     */
+    public static function makeGetMethod($httpMethod, $baseUrl, $apiPath, $params, TiktokShopConfig $apiConfig)
+    {
+
+        // Validate Input
+        /** @var TiktokShopConfig $apiConfig */
+        if ($apiConfig->getAppKey() == "") throw new Exception("Input of [app_key] is empty");
+        if ($apiConfig->getSecretKey() == "") throw new Exception("Input of [secret_key] is empty");
+        if ($apiConfig->getAccessToken() == "") throw new Exception("Input of [access_token] is empty");
+        if($apiPath != "/api/shop/get_authorized_shop")
+        {
+            if ($apiConfig->getShopId() == "") throw new Exception("Input of [shop_id] is empty");
+        }
+
+        //Timestamp
+        $timeStamp = time();
+        $params["timestamp"] = $timeStamp;
+        $params["app_key"] = $apiConfig->getAppKey();
+
+        $signedKey = SignGenerator::generateSign($apiPath, $apiConfig->getSecretKey(), $params);
+
+        $params["sign"] = $signedKey;
+
+        $apiPath .= "?";
+
+        if ($params != null){
+            foreach ($params as $key => $value){
+                $apiPath .= "&". $key . "=" . urlencode($value);
+            }
+        }
+
+        $requestUrl = $baseUrl . $apiPath . "&access_token=" .urlencode($apiConfig->getAccessToken());
+
+        $guzzleClient = new Client([
+            'base_uri' => $baseUrl,
+            'timeout' => $apiConfig->getTimeOut()
+        ]);
+
+        $response = null;
+
+        try
+        {
+            $response = json_decode($guzzleClient->request($httpMethod, $requestUrl)->getBody()->getContents());
+        }
+        catch (\GuzzleHttp\Exception\ClientException $e)
+        {
+            $response = json_decode($e->getResponse()->getBody()->getContents());
+        }
+        catch(\Throwable $e)
+        {
+            $response = (object) array("error" => "GUZZLE_ERROR", "message" => $e->getMessage());
+        }
+
+        return $response;
+    }
+
+}
